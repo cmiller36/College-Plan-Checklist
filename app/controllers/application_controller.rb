8752586acba10f38216777c56d07565b836146ec
cmiller36 <m.calliemiller@gmail.com>
2016-11-30 21:29:40 -0500

    set up basic application & user controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,5 +2,16 @@ require './config/environment'
 
 class ApplicationController < Sinatra::Base
 
+  configure do
+    set :views, "app/views"
+    set :public_folder, 'public'
+    enable :sessions
+    set :session_secret, "password_security"
+  end
+
+  get '/' do
+    erb :index
+  end
+
 
 end
