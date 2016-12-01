4e6a3cab18088e138e4526a4f920297d4f57a520
cmiller36 <m.calliemiller@gmail.com>
2016-12-01 14:16:09 -0500

    update controllers, added navbar, and added views

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -13,5 +13,16 @@ class ApplicationController < Sinatra::Base
     erb :index
   end
 
+  helpers do
+
+    def current_user
+      @current_user ||= User.find_by(id: session[:user_id])
+    end
+
+    def logged_in?
+      !!current_user
+    end
+
+  end
 
 end
--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,55 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if logged_in?
+      redirect to '/my_profile'
+    else
+      erb :'users/signup'
+    end
+  end
+
+  post '/signup' do
+    if params[:username] != "" && params[:email] != "" && params[:password] != ""
+      @user = User.new(username: params[:username], email: params[:email], password: params[:password])
+      @user.save
+      session[:user_id] = @user.id
+      redirect to '/my_profile'
+    else
+      redirect '/signup'
+    end
+  end
+
+  get '/login' do
+    if logged_in?
+      redirect to '/my_profile'
+    else
+      erb :'users/login'
+    end
+  end
+
+  post '/login' do
+      @user = User.find_by(username:params[:username])
+    if @user && @user.authenticate(params[:password])
+      session[:user_id] = @user.id
+      redirect '/my_profile'
+    else
+      redirect '/login'
+    end
+  end
+
+  get '/logout' do
+    if logged_in?
+      session.clear
+    end
+    redirect '/'
+  end
+
+  get '/my_profile' do
+    if logged_in?
+      erb :'users/my_profile'
+    else
+      redirect '/login'
+    end
+  end
 
 end
